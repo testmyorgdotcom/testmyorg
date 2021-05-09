3a01d1c6bb45449eb187f375eef7d9f3b33d840c
baksale <java.rabota@gmail.com>
2021-05-09 21:50:06 +0300

    support for navigation to app

---
+++ src/main/java/org/testmy/screenplay/ui/NavigationBar.java
@@ -0,0 +1,11 @@
+package org.testmy.screenplay.ui;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class NavigationBar {
+
+    public static Target appName() {
+        return Target.the("App Name")
+                .locatedBy("//div[@data-aura-class='oneAppNavContainer']//span[contains(@class, 'appName')]//span");
+    }
+}
