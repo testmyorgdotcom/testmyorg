539c2bef0b01925a0a1083e32e4831c57ddeeef1
baksale <33149951+baksale@users.noreply.github.com>
2021-06-15 23:09:33 +0300

    added initilize framework activity

---
+++ src/main/java/org/testmy/screenplay/act/Initialize.java
@@ -0,0 +1,22 @@
+package org.testmy.screenplay.act;
+
+import org.testmy.screenplay.ability.AbilityProvider;
+import org.testmy.screenplay.ability.InitializeFramework;
+
+import net.serenitybdd.core.steps.Instrumented;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+
+public class Initialize implements Performable {
+    private AbilityProvider abilityProvider = AbilityProvider.getInstance();
+
+    public static Initialize framework() {
+        return Instrumented.instanceOf(Initialize.class).newInstance();
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        final InitializeFramework initializeFrameworkAbility = abilityProvider.as(actor, InitializeFramework.class);
+        initializeFrameworkAbility.loadRecordTypes();
+    }
+}
